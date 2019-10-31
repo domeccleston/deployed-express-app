4d23b04604499b3c179b7932bc464bf0e92e6a95
Dom <domeccleston@gmail.com>
2019-10-31 10:14:47 +0000

    start script

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
+require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const server = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const friends = [
-    {id: 1, name: "Francis"},
-    {id: 2, name: "Damilola"},
-    {id: 3, name: "Amira"},
-]
+  { id: 1, name: 'Francis' },
+  { id: 2, name: 'Damilola' },
+  { id: 3, name: 'Amira' }
+];
 
 server.use(express.json());
 
-server.get("/", (req, res) => {
-    res.json("Deployed Express app")
-})
+server.get('/', (req, res) => {
+  res.json('Deployed Express app');
+});
 
-server.get("/api/friends", (req, res) => {
-    res.json(friends);
-})
+server.get('/api/friends', (req, res) => {
+  res.json(friends);
+});
 
 server.listen(port, () => {
-    console.log("\nServer running on port 3000\n")
-})
+  // eslint-disable-next-line no-console
+  console.log(`\nServer running on port ${port}\n`);
+});
